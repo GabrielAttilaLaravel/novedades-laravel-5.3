f9b8ac96aadf439b384fc0a60661c303e39cfa53
“GabrielMoreno” <gabrieljmorenot@gmail.com>
2017-04-16 01:09:27 -0400

    JavaScript y CSS en Laravel con Elixir, Gulp y Webpack.

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -3,19 +3,19 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Welcome</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+                        Welcome to Gabriel {{ name }}.
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
 <script>
     export default {
+        props: ['name'],
         mounted() {
             console.log('Component mounted.')
         }
